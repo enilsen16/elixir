9e285c688bc1e9b63736f9413e3a658e69461935
Erik Nilsen <enilsen16@live.com>
2016-02-20 15:11:29 -0800

    basic program

---
+++ TheLittleElixir/lib/metex/lib/worker.ex
@@ -0,0 +1,33 @@
+defmodule Metex.Worker do
+
+  def temperature_of(location) do
+    result = url_for(location) |> HTTPoison.get |> parse_response
+    case result do
+      {:ok, temp } ->
+        "#{location}: #{temp} °C"
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  defp url_for(location) do
+    # Used an api key from the tutorial
+    URI.encode("http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=44db6a862fba0b067b1930da0d769e98")
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+end
