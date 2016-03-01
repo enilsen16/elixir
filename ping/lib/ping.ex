4cf8eb6e1d1489115b05e6daacc20623478fbd00
Erik Nilsen <enilsen16@live.com>
2016-03-01 11:51:42 -0800

    ping for imdb

--- ping/lib/ping.ex
+++ ping/lib/ping.ex
@@ -1,7 +1,11 @@
 defmodule Ping do
 
   def main do
-    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get "https://www.wikipedia.org/wiki/Star_Wars"
-     body |> Floki.find(".div-col a") |> Floki.attribute("href") |> Enum.filter(fn(x) -> String.starts_with?(x, "/wiki/") end )
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get "http://www.imdb.com/title/tt3219604/?ref_=tt_rec_tti"
+    body
+      |> Floki.find("#title_recs a")
+      |> Floki.attribute("href")
+      |> Enum.filter(fn(x) -> String.starts_with?(x, "/title/") end )
+      |> List.first
   end
 end
