9d60a319d4bd63498c0811e284af3feecea8b70d
Erik Nilsen <enilsen16@live.com>
2016-05-12 01:27:26 -0700

    add auth

--- rumbl/web/router.ex
+++ rumbl/web/router.ex
@@ -1,13 +1,14 @@
 defmodule Rumbl.Router do
   use Rumbl.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
+pipeline :browser do
+  plug :accepts, ["html"]
+  plug :fetch_session
+  plug :fetch_flash
+  plug :protect_from_forgery
+  plug :put_secure_browser_headers
+  plug Rumbl.Auth, repo: Rumbl.Repo
+end
 
   pipeline :api do
     plug :accepts, ["json"]
@@ -17,6 +18,7 @@ defmodule Rumbl.Router do
     pipe_through :browser # Use the default browser stack
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
