ce9b49f2298cba6ebf9a04155b49cd5d708982f2
Erik Nilsen <enilsen16@live.com>
2016-05-12 02:51:40 -0700

    video resource

---
+++ rumbl/web/models/video.ex
@@ -0,0 +1,25 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    belongs_to :user, Rumbl.User
+
+    timestamps
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:url, :title, :description])
+    |> validate_required([:url, :title, :description])
+  end
+
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn), [conn, conn.params, conn.assigns.current_user])
+  end
+end
